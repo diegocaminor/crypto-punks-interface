f06ac6058f7096f2d39d864dfbf5f8370a456c6f
Diego Camino Reinoso <diego.camino.r@gmail.com>
2023-08-17 22:38:08 -0500

    Setting up connection with Metamask

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./views/Home";
+import { useEffect } from "react";
+import { Route } from "react-router-dom";
+import Home from "./views/home";
+import Web3 from "web3";
 
 function App() {
+  useEffect(() => {
+    if (window.ethereum) {
+      // this is through the Metamask provider
+      // window.ethereum
+      //   .request({
+      //     method: "eth_requestAccounts",
+      //   })
+      //   .then((accounts) => console.log(accounts));
+
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+    }
+  }, []);
+
   return (
     <>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-      </Routes>
+      <Route path="/" exact component={Home} />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </HashRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
